0e7b20a3662b156c5d6c8eb332151e8fd2336a5a
abutnal <45108373+abutnal@users.noreply.github.com>
2020-01-07 18:47:39 +0530

    Add files via upload

---
+++ call_by_reference.js
@@ -0,0 +1,24 @@
+function callByReference(varObj) { 
+  console.log("Inside Call by Reference Method"); 
+  varObj.a = 100; 
+  console.log(varObj); 
+} 
+
+
+let varObj = {a:1};
+console.log("Before Call by Reference Method"); 
+console.log(varObj);
+
+callByReference(varObj) 
+console.log("After Call by Reference Method"); 
+console.log(varObj);
+
+
+// output will be : 
+// --------------- 
+// Before Call by Reference Method 
+// {a: 1} 
+// Inside Call by Reference Method 
+// {a: 100} 
+// After Call by Reference Method 
+// {a: 100}
