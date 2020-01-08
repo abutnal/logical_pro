3c992de135d12e3934655136e62adcf03598116e
abutnal <45108373+abutnal@users.noreply.github.com>
2020-01-09 00:58:16 +0530

    Update perfectnumber.js

--- perfectnumber.js
+++ perfectnumber.js
@@ -1,22 +1,16 @@
-function is_perfect(start, end)
-{
-    var total = 0;
-     
-  for(var i = start; i<=end; i++)
-    {
-        for( var j=1; j<=i/2; j++)
-        {
-            if (!(i%j))
-            {
-                total+=j;
+const isPerfect = (start, end) =>{
+    var count;
+    for(var i=start;i<=end;i++){
+        for(var j=1;j<=i/2;j++){
+            if(i%j==0){
+                count += j;
+                
             }
         }
-        if (i==total)
-        {
+        if(count==i){
             console.log(i);
         }
-        //it works
-        total=0;
+        count = 0;
     }
- } 
-is_perfect(1, 100);
+}
+isPerfect(1,1000);
