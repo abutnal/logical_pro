2395453cb0f8ddc90b948aa77911ce58a42ea52b
abutnal <45108373+abutnal@users.noreply.github.com>
2020-01-09 00:30:03 +0530

    Update removeDuplicate.js

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -31,3 +31,12 @@ console.log(b);
 const a = [1,2,4,4,5,1,2,4,0];
 let Arr = [...new Set(a)];
 console.log(Arr);
+
+// Method 4: show duplicate or show unique
+const arr = [1,2,2,3,3,5,6,7,6];
+var sortedArr = arr.sort();
+sortedArr.filter((ele, index, arr)=>{
+    if(ele == arr[index + 1]){
+        console.log(ele);
+    }
+})
